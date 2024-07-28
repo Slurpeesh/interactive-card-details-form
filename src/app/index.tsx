4817bc2855ea0841c30b3fe26299acaca5c06160
Slurpeesh <JeffNFG@mail.ru>
2024-07-28 22:15:40 +0300

    init commit

---
+++ src/app/index.tsx
@@ -0,0 +1,19 @@
+import App from '@/app/App'
+import { store } from '@/app/store'
+import '@/index.css'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+
+const root = document.getElementById('root')
+
+if (!root) {
+  throw new Error('root not found')
+}
+
+const container = createRoot(root)
+
+container.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
